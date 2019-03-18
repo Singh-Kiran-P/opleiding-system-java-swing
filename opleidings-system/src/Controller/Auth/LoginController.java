7abc0f7cc56edb47b89f58298e02cc2ce8f6dcbb
Singh Kiran <singh.kiran2456@hotmail.com>
2019-03-18 16:23:33 +0100

    login ad register

---
+++ opleidings-system/src/Controller/Auth/LoginController.java
@@ -0,0 +1,36 @@
+package Controller.Auth;
+
+import Controller.Controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LoginController {
+    @FXML
+    public void Login(ActionEvent event) {
+        System.out.println("helllo world");
+    }
+
+    public void Register(ActionEvent event)  throws Exception {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/UI/Views/Auth/Register.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.setTitle("Register");
+            stage.show();
+        } catch(IOException e){
+            Logger logger = Logger.getLogger(getClass().getName());
+            logger.log(Level.SEVERE, "Failed to create new Window.", e);
+        }
+    }
+}
